329ea43e4711dd17c1d37d4bdf943b3320a60b59
ev296 <78567322+ev296@users.noreply.github.com>
2021-05-19 16:50:01 -0500

    Update Horoscope.h

--- PIA/Horoscope.h
+++ PIA/Horoscope.h
@@ -1,3 +1,16 @@
+/*Hello there!! Good that you are in my program, I hope you like it.
+Can be compiled in GDB and CODE BLOKS; For GDB the order is as follows:
+Horoscope. h
+Horoscope. c
+main. c
+(Deleting the 2 #include "Horoscope.h")
+Or you can go back, open the "PIA" file and it all comes together.
+
+And in CODE BLOKS you only need to add the 3 tabs.
+
+It usually takes a while to compile so wait until It welcomes you.
+Luck!!
+*/
 #include <stdio.h>
 #include <stdlib.h>
 int Z;//signo
